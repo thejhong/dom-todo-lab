e76263a40f04e4d5de97b6abb8da4074b0627b18
Jhong <jhong2792@gmail.com>
2022-05-31 18:22:46 -0600

    test 2

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('sanity check')
+//Cached elements
+
+const enterToDO = document.getElementById("enter-todo")
+const submitButton = document.getElementById("submit-button")
+const todoList = document.getElementById("todo-list")
+
+//Event listeners
+
+submitButton.addEventListener('click', function(evt){
+  if(enterToDO.value === '') return
+  const  newToDo = document.createElement('Li')
+  newToDo.textContent = todoInput.value
+  todoList.appendChild(newTodo)
+  enterToDO.value = ''
+})
+
+todoList.addEventListener('click', function(evt){
+const parentNode = event.target.pareentNode
+parentNode.removeChild(event.target)
+})
